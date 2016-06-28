00b09084106819f93d8aae41448ee69918f7e37f
Evan Krambuhl <thekrambuhl@gmail.com>
2016-06-27 21:39:01 -0700

    init

---
+++ README.md
@@ -0,0 +1,15 @@
+# reflecto
+
+
+
+## Install
+
+With [npm](https://www.npmjs.com) do:
+
+```
+npm install reflecto
+```
+
+## License
+
+MIT
---
+++ test/index.test.js
@@ -0,0 +1,7 @@
+var test = require('tape');
+var Reflecto = require('../dist');
+
+test('true', function(t) {
+  t.plan(1);
+  t.equal(true, true);
+});
